35e67c692f7fd7ca7d5f0e790da95d9e15738188
GreatGodson <greatgodsonokezie@yahoo.com>
2022-05-17 09:32:01 +0100

    ⚡️ fix: fix api json format

--- lib/services/remote_service.dart
+++ lib/services/remote_service.dart
@@ -6,18 +6,18 @@ import 'package:new_api/models/coinlist.dart';
 import 'package:flutter/services.dart' show rootBundle;
 
 class RemoteService {
+  static getCoins() async {
+    final response =
+        await http.get(Uri.parse("http://api.coincap.io/v2/assets"));
+    if (response.statusCode == 200) {
+      final posts = jsonDecode(response.body);
 
-
-static getCoins()async{
-  final response = await http.get(Uri.parse("http://api.coincap.io/v2/assets"));
-  if (response.statusCode == 200){
-    final posts = jsonDecode(response.body);
-    print(posts['data'][1]);
-    return posts;
-  }else {
-  throw Exception('Failed to load. OKAY ?');
+      ///instead of returning posts i returned posts['data']
+      /// remember it is a map, and what we need from the api is ['data']
+      /// this is without using a model class
+      return posts['data'];
+    } else {
+      throw Exception('Failed to load. OKAY ?');
+    }
   }
-}
-
-
 }
--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -11,11 +11,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   dynamic coinData;
   var isLoading = false;
-
-
 
   @override
   void initState() {
@@ -23,41 +20,44 @@ class _HomePageState extends State<HomePage> {
     getData();
   }
 
-  void getData()async{
-    var apiResponse = RemoteService.getCoins();
+// assign what is returned from the getCoins method here to the apiResponse
+// and store it in the coin data variable
+  void getData() async {
+    var apiResponse = await RemoteService.getCoins();
     coinData = apiResponse;
-
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Comments"),
-      ),
-
-      body: FutureBuilder<dynamic>(
-        future: RemoteService.getCoins(),
-          builder: (context, snapshot){
-            if(snapshot.hasData){
-              return ListView.separated(
-                itemCount: snapshot.data.length,
-                  itemBuilder: (context, index){
-                    final userData = coinData[index];
-
-                    CoinCap coinPrice = CoinCap(
-                        id: coinData[index]['id'],
-                        rank: coinData[index]['rank'],
-                        symbol: coinData[index]['symbol'],
-                        name: coinData[index]['name'],
-                        supply: coinData[index]['supply'],
-                        maxSupply: coinData[index]['maxSupply'],
-                        marketCapUsd: coinData[index]['marketCapUsd'],
-                        volumeUsd24Hr: coinData[index]['volumeUsd24Hr'],
-                        priceUsd: coinData[index]['priceUsd'],
-                        changePercent24Hr: coinData[index]['changePercent24Hr'],
-                        vwap24Hr: coinData[index]['vwap24Hr']
-                    );
+        appBar: AppBar(
+          title: const Text("Comments"),
+        ),
+        body: FutureBuilder<dynamic>(
+            future: RemoteService.getCoins(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return ListView.separated(
+                  itemCount: snapshot.data.length,
+                  itemBuilder: (context, index) {
+                    /// the problem here was that you returned
+                    /// a map of list of map from getCoins()
+                    /// and tried to access it as a List of map
+                    /// in order to access it as a list of map
+                    /// in stead of returning posts i returned posts['data']
+                    // final userData = coinData[index];
+                    // CoinCap coinPrice = CoinCap(
+                    //     id: coinData[index]['id'],
+                    //     rank: coinData[index]['rank'],
+                    //     symbol: coinData[index]['symbol'],
+                    //     name: coinData[index]['name'],
+                    //     supply: coinData[index]['supply'],
+                    //     maxSupply: coinData[index]['maxSupply'],
+                    //     marketCapUsd: coinData[index]['marketCapUsd'],
+                    //     volumeUsd24Hr: coinData[index]['volumeUsd24Hr'],
+                    //     priceUsd: coinData[index]['priceUsd'],
+                    //     changePercent24Hr: coinData[index]['changePercent24Hr'],
+                    //     vwap24Hr: coinData[index]['vwap24Hr']);
 
                     return Container(
                       padding: const EdgeInsets.all(15),
@@ -65,34 +65,39 @@ class _HomePageState extends State<HomePage> {
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text(coinPrice.name, style: TextStyle(
-                            fontSize: 19,
-                            fontWeight: FontWeight.bold,
+                          Text(
+                            /// because we have tapped into the ['data'] property from the api
+                            /// all we need to tap into now is the index in the list and the
+                            /// property we need from the map
+                            coinData[index]['name'].toString(),
 
-                          ),),
-                          SizedBox(height: 5,),
-                          Text(coinData[index]['id'])
-
+                            style: const TextStyle(
+                              fontSize: 19,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          Text(coinData[index]['id'].toString())
                         ],
                       ),
                     );
                   },
-                  separatorBuilder: (context, index){
+                  separatorBuilder: (context, index) {
                     return const Divider(
                       color: Colors.green,
                     );
                   },
+                );
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
+              return const Center(
+                child: CircularProgressIndicator(
+                  color: Colors.pink,
+                ),
               );
-            }else if(snapshot.hasError){
-              return Text('${snapshot.error}');
-            }
-            return Center(
-              child: const CircularProgressIndicator(
-                color: Colors.pink,
-              ),
-            );
-          })
-
-    );
-}
+            }));
+  }
 }
